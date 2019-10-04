90ebf74ec4cf11bf9210efb266788e0a23dcf9a9
aymar <aymar@DESKTOP-5>
2019-10-04 12:46:13 +0100

    "Added online services component"

--- src/Sec_Frame.py
+++ src/Sec_Frame.py
@@ -4,16 +4,17 @@ Created on 30 Sep 2019
 @author: aymar
 
 '''
-industry = input("Select your industry (Defense /Finance /Public Sector /Charity /Other): ")
+industry = input("Select your industry (Defense /Finance /Public Sector /Charity /Services /Other): ")
 
 question_one = input("Is Data central to your business? (Yes/No): ")
-question_two = input("Is business home or office based? (Yes/No): ")
+question_two = input("Is your business home and office based? (Yes/No): ")
 question_three = input("Is the company size greater than 10? (Yes/No): ")
 
-# q4 = input("Is your solution a Web app /Mobile app /Other? ")
-# q5 = input("Do you use Third parties? (Yes/No)")
+question_four = input("Is your solution a Web app /Mobile app? (Yes/No)")
+question_five = input("Do you use Third parties? (Yes/No)")
+question_six = input("Is registered users greater than 1000? (Yes/No)")
 
-# color = {"Green", "Yellow", "Orange", "Red"}
+color = {"Green", "Yellow", "Orange", "Red"}
 
 if industry == "Defense" :
     if question_one == "Yes" and question_two == "Yes" and question_three == "Yes" :
@@ -39,6 +40,13 @@ elif industry == "Public Sector" :
         print(" Your Security risk: Medium")
     elif question_two == "Yes" :
         print(" Your Security risk: Low")
+    else :
+        print(" Your Security risk: Very Low")
+elif industry == "Online_Services" :
+    if question_one == "Yes" and question_four == "Yes" :
+        print(" Your Security risk: High")
+    elif (question_one == "Yes" and question_three == "Yes") or (question_four == "Yes" and question_two == "Yes") :
+        print(" Your Security risk: Medium")
     else :
         print(" Your Security risk: Very Low")
 elif industry == "Charity" :
